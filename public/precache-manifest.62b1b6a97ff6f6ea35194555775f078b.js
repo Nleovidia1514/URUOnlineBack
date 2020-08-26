4ea99603c00c2e87888da8cb692b15e3f6ce1917
Leonardo Baron <lbaron@conocimientocorporativo.com>
2020-08-25 22:32:07 -0400

    production build

---
+++ public/precache-manifest.62b1b6a97ff6f6ea35194555775f078b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c733749aebf5d6040a2d0a67de2d5be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e1e25800cec320215fc",
+    "url": "/static/css/2.2212e2aa.chunk.css"
+  },
+  {
+    "revision": "6848ede1aae8d1fb4b13",
+    "url": "/static/css/main.cb4067c0.chunk.css"
+  },
+  {
+    "revision": "4e1e25800cec320215fc",
+    "url": "/static/js/2.08cabd65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.08cabd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6848ede1aae8d1fb4b13",
+    "url": "/static/js/main.51487050.chunk.js"
+  },
+  {
+    "revision": "f7d34d977afe3df7d230",
+    "url": "/static/js/runtime-main.d500e83f.js"
+  },
+  {
+    "revision": "960784f815be7eb5de893c95e6e3633c",
+    "url": "/static/media/bg-img1.960784f8.jpg"
+  }
+]);
